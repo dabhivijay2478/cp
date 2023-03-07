3f514a2ab10c966caf3c4d132676c6d822d46b1c
vijaydabhi <dabhivijay2478@gmail.com>
2023-03-07 21:15:01 +0530

    post certficate

--- client/src/component/Common/Postcertificates.jsx
+++ client/src/component/Common/Postcertificates.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export default function Postcertificates() {
+  const [Certificate, setCertificate] = useState("");
+  const [Certificateimg, setCertificateimg] = useState(null);
+  const handlerest = () => {
+    setCertificate("");
+    setCertificateimg(null);
+  };
+  const handleFileChange = (event) => {
+    setCertificateimg(event.target.files[0]);
+  };
   return (
-    <div>Postcertificates</div>
-  )
+    <>
+      <div className="flex justify-center container">
+        <div className="card w-full  bg-white border-double  container  shadow-lg ">
+          <div className="flex px-2 py-3 justify-center">
+            <input
+              type="text"
+              value={Certificate}
+              onChange={(e) => setCertificate(e.target.value)}
+              placeholder="Enter Certificate Info"
+              className="input input-bordered  input-accent w-full max-w-xs mt-2 px-3 py-2 bg-white text-gray-900"
+            />
+          </div>
+          <div className="flex px-2 py-2 bg-white text-gray-900 justify-center">
+            <input
+              type="file"
+              onChange={handleFileChange}
+              className="file-input file-input-bordered file-input-info w-full bg-white text-gray-900 max-w-xs"
+            />
+          </div>
+
+          <div className="px-2 py-2">
+            <button
+              onClick={handlerest}
+              class="px-5 py-2.5 relative rounded group  text-white font-medium inline-block"
+            >
+              <span class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+              <span class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+              <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+              <span class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+              <span class="relative">Reset</span>
+            </button>
+            <button
+              // onClick={Addevent}
+              class="px-5 ml-5 py-2.5 relative rounded group  text-white font-medium inline-block"
+            >
+              <span class="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+              <span class="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+              <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+              <span class="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+              <span class="relative">Add</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
